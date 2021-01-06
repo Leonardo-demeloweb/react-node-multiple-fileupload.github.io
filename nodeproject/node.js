84576487c116a4a807ee9493a19aff5c1b21f4da
Leonardo de Melo <Leonardo de Melo>
2021-01-06 16:54:35 -0300

    descrição do passo a passo de elaboraçã do script de upload da API

--- nodeproject/node.js
+++ nodeproject/node.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload'); //import da biblioteca para file upload
 const cors = require('cors')
 const app = express();
 
-app.use(express.static('public'));
-app.use(cors());
-app.use(fileUpload());
+app.use(express.static('public')); // inicializando método do express para visualizar a pasta public dentro do diretório
+app.use(cors()); // inicializando library que permite acesso de diferentes origens à API
+app.use(fileUpload()); //inicializando a biblioteca para uploadde arquivos
 
+/**
+ * Rota e midleware para checagem e envio de arquivos do frontend para a pasta
+ * public, que se encontra dentro do diretório da API. 
+ * Considerando que esta não seria a melhor arquitetura para escalar a aplicação.
+ * Para este caso o arquivo físico seria armazenado em um cloud storage e a path em um .db
+ */
 app.post('/upload', (req, res) => {
-    if (!req.files) {
-        return res.status(500).send({ msg: "file is not found" })
-    }
+    // condicional que verifica se existem arquivos anexados antes de serem armazenados propriamente
+    if (!req.files) res.status(500).send({ msg: "file is not found" })
+    // variavel em que se armazana a iteração dentro do objeto de cada arquivo
     const myFile = req.files.image;
-    myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+    // função que nomeia e move os respectivos arquivos anexados, para pasta public
+    myFile.mv(`${__dirname}/public/${myFile.name}`, (err) => {
         if (err) {
-            console.log(err)
             return res.status(500).send({ msg: "Error occured" });
         }
         return res.send({ name: myFile.name, path: `/${myFile.name}` });
     });
 })
+
 app.listen(8000, () => {
     console.log('server is running at port 8000');
 })
